6469b4c23d9f9af0743c1e4a39826d8cb4b45dc7
honghuh <honghuh@asb9804u-c06.csil.sfu.ca>
2019-07-09 12:24:23 -0700

    Chagne ffeature of sliding sidebar

--- public/javascript/dynamicstyle.js
+++ public/javascript/dynamicstyle.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
-  $("#togglebutton").click(function(){
-	var toggleWidth = $("#sidebar").width() == 145 ? "15px" : "145px";
-    $('#sidebar').animate({
-      width: toggleWidth
-    });
+  $("#togglebutton").click(function(){  
+      $('#sidebar').animate({
+        width: 'toggle'
+      });
   });
 });
