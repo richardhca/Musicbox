a8650556231f3ac97149bc5585969efc03d5009b
honghuh <honghuh@sfu.ca>
2019-07-17 01:35:53 -0700

    Embed APlayer and fixing colors and layouts

--- app.js
+++ app.js
@@ -27,8 +27,6 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/aplayer',
-        express.static(path.join(__dirname, 'node_modules/aplayer/dist')));
 app.use('/fontawesome', express.static(
     path.join(__dirname + '/node_modules/@fortawesome/fontawesome-free/')));
 
