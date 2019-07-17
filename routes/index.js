03ea71f3918dc3dc54f7c1eecc1d4b2faae8e689
arlenx <arlenx@sfu.ca>
2019-07-17 02:25:57 -0700

    add test image

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,14 @@ const router = express.Router();
 
 const sessionMiddleware = require('../middlewares/sessionMiddleware');
 
-// router.get('/', sessionMiddleware.requiredLogin, function (req, res, next) {
-//     res.render('index');
-// });
-
-// for test
-router.get('/', function (req, res, next) {
+router.get('/', sessionMiddleware.requiredLogin, function (req, res, next) {
     res.render('index');
 });
+
+// for test
+// router.get('/', function (req, res, next) {
+//     res.render('index');
+// });
 
 module.exports = router;
 
