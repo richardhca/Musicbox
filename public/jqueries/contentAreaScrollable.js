a024e651f7357279193dad2b0c72233ad7653100
honghuh <honghuh@sfu.ca>
2019-07-19 21:43:25 -0700

    fix sidebar collapse bug

--- public/jqueries/contentAreaScrollable.js
+++ public/jqueries/contentAreaScrollable.js
@@ -6,11 +6,4 @@ function contentAreaScrollable() {
     const tool_bar_height = $('#tool-bar').innerHeight();
     const music_player_area_height = $('.music-player-area').innerHeight();
     const sidebar_width = $('#sidebar').innerWidth();
-    $('#tool-bar').css({'left': sidebar_width});
-    $('#content-area').css({
-                               'left': sidebar_width,
-                               'top': tool_bar_height,
-                               'bottom': music_player_area_height
-                           });
-
 }
