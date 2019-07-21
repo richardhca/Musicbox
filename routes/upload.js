c8a4803a8491270b4cd82533123c304eb4cfc0e5
Motaz Balghonaim <mo_balghonaim@sfu.ca>
2019-07-21 14:05:25 -0700

    Removed temporary upload view and route

--- routes/upload.js
+++ routes/upload.js
@@ -4,15 +4,10 @@ const sessionMiddleware = require('../middlewares/sessionMiddleware');
 const {upload} = require('../config/multerConfig');
 const uploadController = require('../controllers/uploadController');
 
-
-router.get('/', function (req, res, next) {
-    res.render('upload');
-});
-
 router.post('/',
-    sessionMiddleware.requiredLogin,
-    upload.array('media', 10), // Max upload = 10
-    uploadController.upload_post
+  sessionMiddleware.requiredLogin,
+  upload.array('media', 10), // Max upload = 10
+  uploadController.upload_post
 );
 
 module.exports = router;
