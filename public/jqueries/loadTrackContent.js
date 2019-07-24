343f8600de6b2ef073b10896c8f2aef9eaf06b50
arlenx <arlenx@sfu.ca>
2019-07-23 22:25:25 -0700

    update

--- public/jqueries/loadTrackContent.js
+++ public/jqueries/loadTrackContent.js
@@ -4,53 +4,31 @@ $(document).ready(function () {
         // $('#tool-bar i[id=\'add_playlist_icon\']').attr('id',
         // 'add_track_icon');
         $('#tool-bar i[id=\'add_playlist_icon\']').show();
-        song_detail('GET');
+        tracks_detail('GET');
     });
 
-    //$('#tool-bar').on('click', 'i[id=\'add_track_icon\']', function (event) {
-    //    event.preventDefault();
-    //    song_create_get('GET');
-    //});
 
-    function song_detail(type) {
+    function tracks_detail(type) {
         $.ajax({
-                   type: 'GET',
-                   url: '/track',
-                   dataType: 'html',
-                   data: {info: 'ajax, track detail', type: type},
-                   success: function (result) {
-                       // const state = {id: 1, name: 'song'};
-                       console.log(window.location.href);
-                       console.log(result);
-                       window.history.pushState(null, null, '/track/');
-                       // console.log($(result).filter('div').find('*'));
-                       $('#tool-bar')
-                           .html($(result).filter('#track_tool_bar'));
-                       $('#content-area')
-                           .html($(result).filter('#track_detail'));
-                   },
-                   error: function (e) {
-                       console.log('error: ', e);
-                   }
-               });
+            type: 'GET',
+            url: '/track',
+            dataType: 'html',
+            data: {info: 'ajax, tracks detail', type: type},
+            success: function (result) {
+                // const state = {id: 1, name: 'song'};
+                console.log(window.location.href);
+                console.log(result);
+                window.history.pushState(null, null, '/track/');
+                // console.log($(result).filter('div').find('*'));
+                $('#tool-bar')
+                    .html($(result).filter('#track_tool_bar'));
+                $('#content-area')
+                    .html($(result).filter('#track_detail'));
+            },
+            error: function (e) {
+                console.log('error: ', e);
+            }
+        });
     }
 
-    // function song_create_get(type) {
-    //     $.ajax({
-    //                type: 'GET',
-    //                url: '/track/create',
-    //                dataType: 'html',
-    //                data: {info: 'ajax, track create', type: type},
-    //                success: function (result) {
-    //                    console.log(window.location.href);
-    //                    console.log(result);
-    //                    window.history.pushState(null, null, '/track/create/');
-    //                    $('#content-area').html(result);
-    //
-    //                },
-    //                error: function (e) {
-    //                    console.log('error: ', e);
-    //                }
-    //            });
-    // }
 });
