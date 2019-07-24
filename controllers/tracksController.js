a98fb27224ff3a57b7c8d04eec1c4e8513f6f3b9
arlenx <arlenx@sfu.ca>
2019-07-23 21:57:21 -0700

    combine tracks and track

--- controllers/tracksController.js
+++
@@ -1,8 +0,0 @@
-const connection = require('typeorm').getConnection();
-const path = require('path');
-const pug = require('pug');
-
-exports.tracks_get = async function (req, res, next) {
-    const tracks = await connection.getRepository('Tracks').find({owner_id: req.session.userId});
-    res.send({tracks: tracks});
-};
