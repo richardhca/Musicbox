635e7ea58026ba16b65e04e464741b1a7dd0f2ca
tianhaot <tianhaot@sfu.ca>
2019-07-23 22:15:44 -0700

    Added artist in the response. Still data structure issues

--- controllers/albumController.js
+++ controllers/albumController.js
@@ -2,15 +2,27 @@ const connection = require('typeorm').getConnection();
 
 exports.album_detail_get = async function (req, res, next) {
     const album = await connection.getRepository('Albums').findOne({id: req.params.id});
-    if (album == null){
+    const tracks = await connection.getRepository('Tracks').createQueryBuilder("tracks")
+    	.select("tracks")
+    	.addSelect("Artists.artist_id.artist_name")
+    	.leftJoin("tracks.artist_id", "Artists.artist_id")
+	    .where("tracks.owner_id = :userId and tracks.album_id = :album_id", { userId: req.session.userId, album_id: req.params.id})
+	    .getMany();
+    if (album == null || tracks == null){
 		res.status(404)        // HTTP status 404: NotFound
-		   .send('Not found');
+		   .send({});
     	return;
     }
-    const tracks = await connection.getRepository('Tracks').find({album_id: req.params.id, owner_id: req.session.userId});
+    if (tracks.length <= 0){
+		res.status(404)        // HTTP status 404: NotFound
+		   .send({});
+    	return;
+    }
     let artistSet = new Set();
     for (var track, i = 0; track=tracks[i++];) {
-    	artistSet.add(track.title);
+    	if(track.artist_id != null){
+    		artistSet.add(track.artist_id.artist_name);
+    	}
     }
     artists = Array.from(artistSet);
     res.send({title: album.title, published_on: album.published_on, language: album.language, genres: album.genres, artists: artists, cover: album.cover, tracks: tracks});
