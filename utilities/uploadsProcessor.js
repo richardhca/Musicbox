ce871c66cd0f00f281caba51c53bd6e801744713
Motaz Balghonaim <mo_balghonaim@sfu.ca>
2019-07-26 02:16:55 -0700

    Added logic to process albums

--- utilities/uploadsProcessor.js
+++ utilities/uploadsProcessor.js
@@ -45,47 +45,86 @@ const processTrack = async function (metadata, uploader) {
         uploaded_on: new Date(),
         file_name: metadata.common.fileName,
         cover_art_file_name: metadata.common.coverArtFileName,
-        owner_id: uploader
+        owner_id: uploader,
+        genres: metadata.common.genre,
     };
     const tracksRepo = connection.getRepository("Tracks");
-    await tracksRepo.save(newTrackData).catch(error => {
+    return await tracksRepo.save(newTrackData).catch(error => {
         console.log(`Failed to save track. Error: ${error}`)
     });
 };
 
-const processArtist = function (metadata, track) {
-    // TODO
-};
 
-const processAlbum = function (metadata, track, artist) {
-    // TODO
+/*
+ Scenario 1: If album exists, add track to it and update relevant fields.
+ Scenario 2: If album doesn't exist, create a new one.
+ */
+const processAlbum = async function (metadata, track, uploader) {
+
+    // Fields that identify a unique album
+    const albumTitle = metadata.common.album;
+    const artist = track.artist_name;
+    const uploaderId = uploader.id;
+
+    // If uploaded track has an album field
+    if (albumTitle) {
+        const albumsRepo = connection.getRepository("Albums");
+
+        // Find album and load its tracks
+        const album = await albumsRepo
+            .createQueryBuilder("album")
+            .where("LOWER(album.title) = LOWER(:albumTitle)", {albumTitle})
+            .andWhere("LOWER(album.artist_name) = LOWER(:artist)", {artist})
+            .andWhere("album.owner_id = :uploaderId", {uploaderId})
+            .leftJoinAndSelect("album.tracks", "tracks")
+            .getOne();
+
+        // If album exists, update it with new track
+        if (album) {
+            // Add track rank (1 + album's last track's rank)
+            track.rank_in_album = album.tracks.slice(-1)[0].rank_in_album + 1;
+            album.tracks.push(track);
+            album.artists = Array.from(new Set(album.artists.concat(track.artists))); // Set to remove duplicates
+            await albumsRepo.save(album);
+        } else {
+            // Set initial track rank
+            track.rank_in_album = 1;
+            // Create new album
+            await albumsRepo.save({
+                title: albumTitle,
+                artist_name: track.artist_name,
+                artists: track.artists,
+                uploaded_on: track.uploaded_on,
+                owner_id: uploaderId,
+                tracks: [track]
+            });
+        }
+    }
 };
 
 // Process uploaded track
-const processUpload = async function (file, uploaderId) {
+const processUpload = async function (file, uploader) {
     // Extract metadata
     const metadata = await extractMetaData(file);
 
     // Save cover art
     await processCoverArt(metadata);
 
-    // Track uploader
-    const uploader = await connection.getRepository("Users").findOne({id: uploaderId});
-
     // Process Track
     const track = await processTrack(metadata, uploader);
 
-    // Process Artist
-    const artist = await processArtist(metadata, track);
-
     // Process Album
-    const album = await processAlbum(metadata, track, artist);
+    const album = await processAlbum(metadata, track, uploader);
 };
 
 // Process each upload and fetch current logged in user object
 const processUploads = async function (req) {
+
+    // Track(s) uploader
+    const uploader = await connection.getRepository("Users").findOne({id: req.session.userId});
+
     for (var i = 0; i < req.files.length; i++) {
-        processUpload(req.files[i], req.session.userId);
+        await processUpload(req.files[i], uploader);
     }
 };
 
