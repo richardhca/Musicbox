979a25691084128e11772ada6645d269c51725dd
tianhaot <tianhaot@sfu.ca>
2019-07-25 19:38:03 -0700

    Changed the route to match Mo's new db model

--- controllers/albumController.js
+++ controllers/albumController.js
@@ -1,68 +1,63 @@
 const connection = require('typeorm').getConnection();
 
 exports.album_detail_get = async function (req, res, next) {
-	const albumId = parseInt(req.params.id);
-	if (isNaN(albumId)) {
+    const albumId = parseInt(req.params.id);
+    if (isNaN(albumId)) {
         return res.send({});
     }
-    const album = await connection.getRepository('Albums').findOne({id: albumId});
-    const tracks = await connection.getRepository('Tracks').createQueryBuilder("tracks")
-    	.select("tracks")
-    	.addSelect("Artists.artist_id.artist_name")
-    	.leftJoin("tracks.artist_id", "Artists.artist_id")
-	    .where("tracks.owner_id = :userId and tracks.album_id = :album_id", { userId: req.session.userId, album_id: albumId})
-	    .getMany();
+    const album = await connection.getRepository('Albums').findOne({id: albumId, owner_id: req.session.userId});
+    const tracks = await connection.getRepository('Tracks').find({album_id: albumId, owner_id: req.session.userId});
     if (album == null || tracks == null){
-		return res.status(404)        // HTTP status 404: NotFound
-		          .send({});
+        return res.status(404)        // HTTP status 404: NotFound
+                  .send({});
     }
     if (tracks.length <= 0){
-		return res.status(404)        // HTTP status 404: NotFound
-		          .send({});
+        return res.status(404)        // HTTP status 404: NotFound
+                  .send({});
     }
-    let artistSet = new Set();
-    for (var track, i = 0; track=tracks[i++];) {
-    	if(track.artist_id != null){
-    		artistSet.add(track.artist_id.artist_name);
-    	}
-    }
-    artists = Array.from(artistSet);
-    res.send({title: album.title, published_on: album.published_on, language: album.language, genres: album.genres, artists: artists, cover: album.cover, tracks: tracks});
+    res.send({title: album.title, artist_name: album.artist_name, artists: album.artists, published_on: album.published_on, language: album.language, genres: album.genres, cover: album.cover, tracks: tracks});
 };
 
 exports.album_delete = async function (req, res, next) {
-	const otherUserTracks = await connection.getRepository('Tracks').createQueryBuilder("tracks")
-    	.select("tracks")
-    	.where("tracks.owner_id != :userId", { userId: req.session.userId })
-    	.andWhere("tracks.album_id = :id", { id: req.params.id })
-    	.getMany();
+    const albumId = parseInt(req.params.id);
+    if (isNaN(albumId)) {
+        return res.send({});
+    }
+    const otherUserTracks = await connection.getRepository('Tracks').createQueryBuilder("tracks")
+        .select("tracks")
+        .where("tracks.owner_id != :userId", { userId: req.session.userId })
+        .andWhere("tracks.album_id = :id", { id: albumId })
+        .getMany();
     const thisUserTracks = await connection.getRepository('Tracks').createQueryBuilder("tracks")
-    	.select("tracks")
-    	.where("tracks.owner_id = :userId", { userId: req.session.userId })
-    	.andWhere("tracks.album_id = :id", {id: req.params.id})
-    	.getMany();
+        .select("tracks")
+        .where("tracks.owner_id = :userId", { userId: req.session.userId })
+        .andWhere("tracks.album_id = :id", {id: albumId })
+        .getMany();
+    const thisUserAlbum = await connection.getRepository('Albums').findOne({id: albumId, owner_id: req.session.userId});
     if (otherUserTracks.length > 0){
-    	res.status(403)
-    	   .send("403 Forbidden");
-    	return;
+        return res.status(403)
+           .send("404 Not Found");
     }
     if(thisUserTracks.length <= 0){
-    	res.status(404)
-    	    .send("404 Not Found");
-    	return;
+        return res.status(404)
+            .send("404 Not Found");
     }
-	await connection.getRepository('Tracks')
-	    .createQueryBuilder()
-	    .delete()
-	    .from('Tracks')
-	    .where("album_id = :id", {id: req.params.id})
-	    .andWhere("owner_id = :userId", {userId: req.session.userId})
-	    .execute();
-	await connection.getRepository('Albums')
-	    .createQueryBuilder()
-	    .delete()
-	    .from('Albums')
-	    .where("id = :id", {id: req.params.id})
-	    .execute();
-	res.send("Success");
+    if(thisUserAlbum == null){
+    	return res.status(404).send("404 Not Found");
+    }
+    await connection.getRepository('Tracks')
+        .createQueryBuilder()
+        .delete()
+        .from('Tracks')
+        .where("album_id = :id", {id: albumId})
+        .andWhere("owner_id = :userId", {userId: req.session.userId})
+        .execute();
+    await connection.getRepository('Albums')
+        .createQueryBuilder()
+        .delete()
+        .from('Albums')
+        .where("id = :id", {id: albumId})
+        .andWhere("owner_id = :userId", {userId: req.session.userId})
+        .execute();
+    res.send("Success");
 };
