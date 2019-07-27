184bfa5e64e841c6cd3ef6cc8da14ebad9de666a
honghuh <honghuh@sfu.ca>
2019-07-26 21:07:15 -0700

    Add create empty playlist feature

--- routes/playlist.js
+++ routes/playlist.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser');
 const router = express.Router();
 
 const playlist_controller = require('../controllers/playlistController');
@@ -8,7 +10,15 @@ const playlistMiddleware = require('../middlewares/playlistMiddleware');
 router.get('/', sessionMiddleware.requiredLogin, playlistMiddleware.getUserPlaylistInfo,
     playlist_controller.playlist_page_get);
 
-router.get('/create', sessionMiddleware.requiredLogin, playlist_controller.playlist_create_get);
+router.post('/create', function(req, res) { 
+	var filename = req.body.PlaylistName;
+	fs.writeFile('./public/playlists/'+filename, '', function(err) {
+		if (err) throw err;
+		console.log('Playlist Created.');
+	});
+	res.status(200).end();
+	res.redirect('/playlist');
+});
 
 router.get('/detail', sessionMiddleware.requiredLogin, playlist_controller.playlist_detail_get);
 
