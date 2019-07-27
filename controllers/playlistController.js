7822451898176485ff9b53c4966a19fbc39b797b
Motaz Balghonaim <mo_balghonaim@sfu.ca>
2019-07-27 14:52:04 -0700

    Updated Playlist controllers

--- controllers/playlistController.js
+++ controllers/playlistController.js
@@ -59,70 +59,98 @@ exports.playlist_create_get = (req, res, next) => {
     }
 };
 
-exports.playlist_details_get = async function (req, res, next) {
-	const playlist = await connection.getRepository("Playlists").findOne({playlist_id: req.params.id, owner_id: req.session.userId});
-	if (playlist==null){
-		return res.status(404).send("404 Not Found");
-	}
-    const tracks = await connection.getRepository("Playlists")
-        .createQueryBuilder("playlists")
-        .select("tracks")
-        .leftJoin("playlist_to_track", "playlist_to_track", "playlist_to_track.playlist_id = playlists.playlist_id")
-        .leftJoin("tracks", "tracks", "playlist_to_track.track_id = tracks.id")
-        .where("playlists.owner_id = :userId", {userId: req.session.userId})
-        .andWhere("playlists.playlist_id = :id", {id: req.params.id})
-        .andWhere("tracks.owner_id = :userId", {userId: req.session.userId})
-        .addOrderBy("playlist_to_track.rank", "ASC")
-        .getRawMany();
-    res.send({name: playlist.name, tracks: tracks});
+// playlist -> playlist_tracks -> tracks TO playlist -> tracks
+const transformPlaylistTracks = function (playlist) {
+    const tracks = [];
+    playlist.playlist_tracks.forEach(playlistTrack => {
+        const track = playlistTrack.track_id;
+        track.rank_in_playlist = playlistTrack.rank;
+        tracks.push(track);
+    });
+    playlist.tracks = tracks;
+    delete playlist.playlist_tracks;
+    playlist.tracks.sort((a, b) => a.rank_in_playlist - b.rank_in_playlist);
 };
 
-exports.playlist_modify_post = async function (req, res, next) {
-	const playlist = await connection.getRepository("Playlists").findOne({playlist_id: req.body.playlistId, owner_id: req.session.userId});
-	const track_record = await connection.getRepository("Playlist_to_track").findOne({playlist_id: req.body.playlistId, track_id: req.body.updateId});
-	console.log(track_record);
-	var newRank = null;
-	if (playlist == null || track_record == null){
-		return res.status(404).send("404 Not Found");
-	}//If no playlist found, return 404
-	//handle the two cases where there is no tracks before or after the track (i.e. move to the top or bottom of the playlist)
-	if(req.body.beforeId == -1){ //If beforeId in the request is -1, that means no track should be before this track (i.e. top of the playlist). Please set the afterId to anything that is not -1
-		const smallestRank = await connection.getRepository("Playlists")
-		    .createQueryBuilder("Playlist_to_track")
-		    .select("MIN(Playlist_to_track.rank)")
-		    .from("playlist_to_track")
-		    .where("playlist_to_track.playlist_id = :playlistId", {playlistId: req.body.playlistId})
-		    .getRawOne();
-	    if(track_record.rank == smallestRank.min){
-			return res.send("Success");
-		}
-		newRank = smallestRank.min / 2;
-	}
-	else if(req.body.afterId == -1){ //If beforeId in the request is -1, that means no track should be after this track (i.e. bottom of the playlist). Please set the beforeId to anything that is not -1
-		const largestRank = await connection.getRepository("Playlists")
-		    .createQueryBuilder("Playlist_to_track")
-		    .select("MAX(Playlist_to_track.rank)")
-		    .from("playlist_to_track")
-		    .where("playlist_to_track.playlist_id = :playlistId", {playlistId: req.body.playlistId})
-		    .getRawOne();
-		if(track_record.rank == largestRank.max){//if the current track is already at the bottom of the playlist. do nothing.
-			return res.send("Success");
-		}
-		newRank = largestRank.max + 1;
-	}
-	else { //If nothing else, put the track between the track indicated in beforeId and afterId
-		const trackBefore = await connection.getRepository("Playlist_to_track").findOne({playlist_id: req.body.playlistId, track_id: req.body.beforeId});
-		const trackAfter = await connection.getRepository("Playlist_to_track").findOne({playlist_id: req.body.playlistId, track_id: req.body.afterId});
-		newRank = (trackBefore.rank+trackAfter.rank)/2;
-	}
-	
-	await connection.getRepository("Playlist_to_track")
-	    .createQueryBuilder("Playlist_to_track")
-	    .update("Playlist_to_track")
-	    .set({rank:newRank})
-	    .where("track_id = :updateId", {updateId: req.body.updateId})
-	    .andWhere("playlist_id = :playlistId", {playlistId: req.body.playlistId})
-	    .execute();
-	return res.send("Success");
+exports.playlist_details_get = async function (req, res, next) {
 
-}
+    // Get playlist and load its tracks
+    const playlist = await connection.getRepository("Playlists").findOne(
+        {
+            playlist_id: req.params.id,
+            owner_id: req.session.userId
+        },
+        {
+            relations: ['playlist_tracks'],
+        }
+    );
+
+    if (!playlist) {
+        return res.status(404).send();
+    }
+
+    transformPlaylistTracks(playlist);
+
+    res.send(playlist);
+};
+
+// TODO: Make sure this works and possibly use query string params instead of body params
+exports.playlist_modify_post = async function (req, res, next) {
+    const playlist = await connection.getRepository("Playlists").findOne({
+        playlist_id: req.body.playlistId,
+        owner_id: req.session.userId
+    });
+    const track_record = await connection.getRepository("Playlist_to_track").findOne({
+        playlist_id: req.body.playlistId,
+        track_id: req.body.updateId
+    });
+    console.log(track_record);
+    var newRank = null;
+    if (playlist == null || track_record == null) {
+        return res.status(404).send("404 Not Found");
+    }//If no playlist found, return 404
+    //handle the two cases where there is no tracks before or after the track (i.e. move to the top or bottom of the playlist)
+    if (req.body.beforeId == -1) { //If beforeId in the request is -1, that means no track should be before this track (i.e. top of the playlist). Please set the afterId to anything that is not -1
+        const smallestRank = await connection.getRepository("Playlists")
+            .createQueryBuilder("Playlist_to_track")
+            .select("MIN(Playlist_to_track.rank)")
+            .from("playlist_to_track")
+            .where("playlist_to_track.playlist_id = :playlistId", {playlistId: req.body.playlistId})
+            .getRawOne();
+        if (track_record.rank == smallestRank.min) {
+            return res.send("Success");
+        }
+        newRank = smallestRank.min / 2;
+    } else if (req.body.afterId == -1) { //If beforeId in the request is -1, that means no track should be after this track (i.e. bottom of the playlist). Please set the beforeId to anything that is not -1
+        const largestRank = await connection.getRepository("Playlists")
+            .createQueryBuilder("Playlist_to_track")
+            .select("MAX(Playlist_to_track.rank)")
+            .from("playlist_to_track")
+            .where("playlist_to_track.playlist_id = :playlistId", {playlistId: req.body.playlistId})
+            .getRawOne();
+        if (track_record.rank == largestRank.max) {//if the current track is already at the bottom of the playlist. do nothing.
+            return res.send("Success");
+        }
+        newRank = largestRank.max + 1;
+    } else { //If nothing else, put the track between the track indicated in beforeId and afterId
+        const trackBefore = await connection.getRepository("Playlist_to_track").findOne({
+            playlist_id: req.body.playlistId,
+            track_id: req.body.beforeId
+        });
+        const trackAfter = await connection.getRepository("Playlist_to_track").findOne({
+            playlist_id: req.body.playlistId,
+            track_id: req.body.afterId
+        });
+        newRank = (trackBefore.rank + trackAfter.rank) / 2;
+    }
+
+    await connection.getRepository("Playlist_to_track")
+        .createQueryBuilder("Playlist_to_track")
+        .update("Playlist_to_track")
+        .set({rank: newRank})
+        .where("track_id = :updateId", {updateId: req.body.updateId})
+        .andWhere("playlist_id = :playlistId", {playlistId: req.body.playlistId})
+        .execute();
+    return res.send("Success");
+
+};
