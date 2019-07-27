3f7360c784976f2f8e172f05f6a7539152095d25
yiminz <yiminz@sfu.ca>
2019-07-27 12:06:13 -0700

    merge playlist routers into playlist.js

--- routes/playlist.js
+++ routes/playlist.js
@@ -9,8 +9,17 @@ const playlistMiddleware = require('../middlewares/playlistMiddleware');
 router.get('/', playlistMiddleware.getUserPlaylistInfo, sessionMiddleware.requiredLogin,
            album_controller.playlist_detail);
 
-//go to playlist create page?
-router.get('/create', album_controller.playlist_create_get);
+router.get('/create', sessionMiddleware.requiredLogin, album_controller.playlist_create_get);
+
+router.get('/add', sessionMiddleware.requiredLogin, album_controller.playlist_send);
+
+router.get('/check', sessionMiddleware.requiredLogin, album_controller.playlist_valid);
+
+router.get('/delete', sessionMiddleware.requiredLogin, album_controller.playlist_delete);
+
+router.get('/modify', sessionMiddleware.requiredLogin, album_controller.playlist_modify);
+
+router.get('/:id/detail', sessionMiddleware.requiredLogin, album_controller.playlist_details_get);
 
 router.get('/:id/detail', sessionMiddleware.requiredLogin, album_controller.playlist_details_get);
 
