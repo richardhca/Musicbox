8b9d3e82935bd78607f72be495c8ce175f72c071
arlenx <arlenx@sfu.ca>
2019-07-27 17:51:26 -0700

    add dropdown menu and fix the bugs of hovering track list

--- public/jqueries/trackListEventHandler.js
+++ public/jqueries/trackListEventHandler.js
@@ -1,29 +1,39 @@
 $(document).ready(function () {
-    $('.track_list').on('mouseover', function () {
-        const forced_music_icon = $(this).find('.tracklist_icon');
+    // $('.track_list_more_icon').hide();
+
+    console.log('document ready');
+
+    $('.track_list').on('mouseenter', function () {
+        console.log('mouse enter');
+        const forced_music_icon = $(this).find('.track_list_icon');
         if (forced_music_icon.html().includes('music_note')) {
             forced_music_icon.html('<i class="material-icons text-white noselect">play_arrow</i>');
         }
         else if (forced_music_icon.html().includes('volume_up')) {
             forced_music_icon.html('<i class="material-icons text-white noselect">pause</i>');
         }
+        $(this).toggleClass('track_hover');
+        $(this).find('.track_list_more_icon').toggle();
     });
 
-    $('.track_list').on('mouseout', function () {
-        const forced_music_icon = $(this).find('.tracklist_icon');
+    $('.track_list').on('mouseleave', function () {
+        console.log('mouse leave');
+        const forced_music_icon = $(this).find('.track_list_icon');
         if (forced_music_icon.html().includes('play_arrow')) {
             forced_music_icon.html('<i class="material-icons text-white noselect">music_note</i>');
         }
         else if (forced_music_icon.html().includes('pause')) {
             forced_music_icon.html('<i class="material-icons text-white noselect">volume_up</i>');
         }
+        $(this).toggleClass('track_hover');
+        $(this).find('.track_list_more_icon').toggle();
     });
 
     $('.track_list').on('dblclick', function (event) {
         event.preventDefault();
 
         // replace icon
-        const forced_music_icon = $(this).find('.tracklist_icon');
+        const forced_music_icon = $(this).find('.track_list_icon');
         // const pretty = html_beautify(forced_music_icon.html());
         // console.log(pretty);
 
@@ -43,7 +53,7 @@ $(document).ready(function () {
 
         // change playicon to music icon
         $(this).siblings('.track_list').each(function () {
-            const icon = $(this).find('.tracklist_icon');
+            const icon = $(this).find('.track_list_icon');
             // console.log(html_beautify($(this).html()));
             if (icon.html().includes('volume_up')) {
                 icon.html('<i class="material-icons text-white noselect">music_note</i>');
@@ -51,4 +61,9 @@ $(document).ready(function () {
             }
         });
     });
+
+    // $('.track_list_more_icon').on('click', function () {
+    //     console.log('more click');
+    //     $('.dropdown-menu').toggle();
+    // });
 });
