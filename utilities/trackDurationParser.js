eb3b151376483ea21cbd486200c004a37761da25
Motaz Balghonaim <mo_balghonaim@sfu.ca>
2019-08-02 23:16:43 -0700

    Removed owner_ids from tracks and used parseDuration

--- utilities/trackDurationParser.js
+++ utilities/trackDurationParser.js
@@ -1,31 +1,36 @@
 const durationParser = function (duration) {
-    var hours   = Math.floor(duration / 3600);
-    var minutes = Math.floor((duration - (hours * 3600)) / 60);
-    var seconds = Math.floor(duration - (hours * 3600) - (minutes * 60));
+    if (duration) {
+        var hours = Math.floor(duration / 3600);
+        var minutes = Math.floor((duration - (hours * 3600)) / 60);
+        var seconds = Math.floor(duration - (hours * 3600) - (minutes * 60));
 
-    if (hours === 0 && minutes === 0) {
+        if (hours === 0 && minutes === 0) {
+            if (seconds < 10) {
+                return minutes + ":" + "0" + seconds;
+            } else {
+                return minutes + ":" + seconds;
+            }
+        }
+
+        if (hours === 0) {
+            if (seconds < 10) {
+                return minutes + ":" + "0" + seconds;
+            } else {
+                return minutes + ":" + seconds;
+            }
+        }
+
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
         if (seconds < 10) {
-            return minutes + ":" + "0" + seconds;
+            seconds = "0" + seconds;
         }
-        else {
-            return minutes + ":" + seconds;
-        }
+        return hours + ':' + minutes + ':' + seconds;
     }
-
-    if (hours === 0) {
-        if (seconds < 10) {
-            return minutes + ":" + "0" + seconds;
-        }
-        else {
-            return minutes + ":" + seconds;
-        }
-    }
-
-    if (minutes < 10) {minutes = "0"+minutes;}
-    if (seconds < 10) {seconds = "0"+seconds;}
-    return hours+':'+minutes+':'+seconds;
+    return '';
 };
 
 module.exports = {
-    durationParser : durationParser
+    durationParser: durationParser
 };
