d54607755c0519c667dafc6d8c71615c4c57b0d4
tianhaot <tianhaot@sfu.ca>
2019-08-05 00:41:08 -0700

    Modified the output for better UX

--- initialize.sh
+++ initialize.sh
@@ -1,4 +1,4 @@
 pass=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-echo $pass
+echo The randomly generated password is: $pass
 sed -i -e 's/POSTGRES_PASSWORD: password/POSTGRES_PASSWORD: '$pass'/g' docker-compose.yml
 sed -i -e 's/password: \"password\"/password: \"'$pass'\"/g' config/createConnection.js
