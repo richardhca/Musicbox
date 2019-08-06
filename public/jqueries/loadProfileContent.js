7737b0226175195f53e3638d8f1cd9c974d1ee2c
honghuh <honghuh@sfu.ca>
2019-08-05 21:36:00 -0700

    Add view and edit profile feature

--- public/jqueries/loadProfileContent.js
+++ public/jqueries/loadProfileContent.js
@@ -4,6 +4,50 @@ $(document).ready(function () {
 		profile_get();
 	});
 	
+	$('#editProfile').on('click', function () {
+		$('.userinfo').prop('disabled', false);
+		$('#saveEdit').css('display', 'inline-block');
+		$('#editProfile').css('display', 'none');
+		$('#changePw').css('display', 'none');
+		$('#cancel').css('display', 'inline-block');
+	});
+	
+	$('#saveEdit').on('click', function () {
+		$('.userinfo').prop('disabled', true);
+		$('#editProfile').css('display', 'inline-block');
+		$('#saveEdit').css('display', 'none');
+		$('#changePw').css('display', 'inline-block');
+		$('#cancel').css('display', 'none');
+	});
+	
+	$('#changePw').on('click', function () {
+		$('.profile_info').css('display', 'none');
+		$('.profile_changePw').css('display', 'block');
+		$('#editProfile').css('display', 'none');
+		$('#changePw').css('display', 'none');
+		$('#savePw').css('display', 'inline-block');
+		$('#cancel').css('display', 'inline-block');
+	});
+	
+	$('#savePw').on('click', function () {
+		$('.profile_changePw').css('display', 'none');
+		$('.profile_info').css('display', 'block');
+		$('#editProfile').css('display', 'inline-block');
+		$('#changePw').css('display', 'inline-block');
+		$('#savePw').css('display', 'none');
+		$('#cancel').css('display', 'none');
+	});
+	
+	$('#cancel').on('click', function () {
+		$('.profile_changePw').css('display', 'none');
+		$('.profile_info').css('display', 'block');
+		$('#editProfile').css('display', 'inline-block');
+		$('#saveEdit').css('display', 'none');
+		$('#changePw').css('display', 'inline-block');
+		$('#savePw').css('display', 'none');
+		$('#cancel').css('display', 'none');
+	});
+	
 	function profile_get() {
         $.ajax({
             type: 'GET',
